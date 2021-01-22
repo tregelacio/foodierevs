5aff1cdc3a8f48f90dc3b731d3d07f548e0f6396
tregelacio <tre.gelacio@gmail.com>
2021-01-22 09:18:50 -1000

    Added thymeleaf template for starting

---
+++ initial/src/main/java/foodierevsapp/Application.java
@@ -0,0 +1,17 @@
+package foodierevsapp;
+
+// Application runs by using './mvnw spring-boot:run' in terminal
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+
+        GreetingWebClient gwc = new GreetingWebClient();
+        System.out.println(gwc.getResult());
+    }
+}
